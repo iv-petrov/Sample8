e59a341e05fb791cc0357a098f4e5ba6506ef528
Петров Игорь <igvapetrov@inno.tech>
2025-04-24 11:47:23 +0300

    Задание 8 - чистая архитектура. Структура слоёв описана в Readme.md

---
+++ DataModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sample8.DataModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Sample8
 Пример 8 (.netcore 2)
+
+Domain - слой домена
+Controllers - слой приложения
+DataModels - слой данных
+Views - слой представлений
